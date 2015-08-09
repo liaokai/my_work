3f78c4190ef155fdee7ada87161d289fde4227c9
wanghaoqiang <308681282@qq.com>
2015-08-09 23:53:42 +0800

    算法

---
+++ alsg/src/com/wgqq/a1/Question1.java
@@ -0,0 +1,14 @@
+package com.wgqq.a1;
+/**
+ * 给定一个字符串，判断他的每一个字母是否是独一无二的
+ * @author xiaoqiang
+ *
+ */
+public class Question1 {
+	
+	
+	public static void main(String[] args) {
+		String [] words={"abcde", "hello", "apple", "kite", "padle"};
+		
+	}
+}
